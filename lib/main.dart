084ba05c096031b3539a652b89e1bb13707b8ffa
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-11-03 21:02:14 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp());//
 }
 
 class MyApp extends StatelessWidget {
